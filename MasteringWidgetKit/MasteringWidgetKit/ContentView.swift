30dcf218bd5b61dc96c09f5266fb22832e776773
koala panda <koakoa8823@protonmail.com>
2023-12-14 23:31:20 +0900

    プロジェクトの追加

---
+++ MasteringWidgetKit/MasteringWidgetKit/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MasteringWidgetKit
+//
+//  Created by koala panda on 2023/09/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("ウィジェットを追加してください")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
